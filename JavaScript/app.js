440df55f5b1a827b83fbe17cbd0296832176f73f
Stanislav <xblueexorcistx@gmail.com>
2021-02-14 17:41:43 +0100

    5-ый коммит

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,20 +1,15 @@
-let menu = {
-  width: 200,
-  height: 150,
-  title: 'my menu'
-};
-
-function multiplayNumeric(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] == 'number') {
-      obj[key] *= 2
-    }
-  }
-  console.log(obj);
-}
-
-multiplayNumeric(menu)
-const pi = 3.14
-let wer = 2
-console.log(wer);
+let money = prompt('Ваш бюджет на месяц');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let question1 = prompt('Введите обязательную статью доходов');
+let question2 = prompt('Во сколько обойдется');
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {question1 : question2
+  },
+  optionalExpenses: '',
+  income: [],
+  savings: false
+}; 
+console.log(alert(money / 30));
 
